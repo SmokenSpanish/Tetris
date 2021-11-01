bd556e7677823ef24c68d3ba494e7c35f60246e5
Maksim Basov <maksimbasov1992@mail.ru>
2021-11-01 16:31:14 +0300

    feat start game

---
+++ main.js
@@ -0,0 +1,27 @@
+const cvs = document.getElementById('game');
+const ctx = cvs.getContext('2d');
+
+const game = {
+    cellSize: 30,
+    playField: [],
+    rowCount: 20,
+    colCount: 10,
+    startGame() {
+        this.initPlayField()
+    },
+    initPlayField() {
+        const { rowCount, colCount } = this;
+
+        this.playField = []
+        for (let row = 0; row < rowCount; row++) {
+            const tmpRow = []
+            for (let col = 0; col < colCount; col++) {
+                tmpRow.push(0)
+            }
+            this.playField.push(tmpRow)
+        }
+    }
+}
+
+game.startGame()
+console.log(game)
